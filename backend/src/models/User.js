e9429d9e613f399f56ed8fe05fb32373f06bdd1d
Hwarang Choi <az8606@naver.com>
2019-06-26 08:58:20 +0900

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -11,16 +11,14 @@ export const User = (sequelize, DataTypes) => {
             unique : true
         },
         password : {
-            type : DataTypes.STRING,
+            type : DataTypes.TEXT,
             allowNull : false
         },
         auth : {
-            type : DataTypes.INTEGER,
-            allowNull : false
+            type : DataTypes.INTEGER
         },
         number : {
-            type : DataTypes.INTEGER,
-            allowNull : true
+            type : DataTypes.STRING(4)
         },
         name : {
             type : DataTypes.STRING(50),
